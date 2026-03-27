50bc4198b7e6b5ae9e47fcc656185a77d11c6a2f
Shalika Neranjana <shalika4net@gmail.com>
2026-03-27 10:24:26 +0530

    Update myInternships.js

--- client/src/api/myInternships.js
+++ client/src/api/myInternships.js
@@ -1,68 +1,72 @@
-import axios from 'axios'
+import api from './axios'
 
-const BASE = 'http://localhost:5000/api'
+const BASE = '/my-internships'
+const DIARY_BASE = '/diary'
+const TASKS_BASE = '/tasks'
+const MONTHLY_REPORTS_BASE = '/monthly-reports'
+const FINAL_REPORTS_BASE = '/final-reports'
 
 // Internships 
 export const getInternships = (studentIdNumber) =>
-  axios.get(`${BASE}/internships/student/${studentIdNumber}`)
+  api.get(`${BASE}/student/${studentIdNumber}`)
 
 export const addInternship   = (data) =>
-  axios.post(`${BASE}/internships`, data)
+  api.post(BASE, data)
 
 export const updateInternship = (id, data) =>
-  axios.put(`${BASE}/internships/${id}`, data)
+  api.put(`${BASE}/${id}`, data)
 
 export const deleteInternship = (id) =>
-  axios.delete(`${BASE}/internships/${id}`)
+  api.delete(`${BASE}/${id}`)
 
 // Daily Diary
 export const getEntries    = (internshipId) =>
-  axios.get(`${BASE}/diary/internship/${internshipId}`)
+  api.get(`${DIARY_BASE}/internship/${internshipId}`)
 
 export const addEntry      = (data) =>
-  axios.post(`${BASE}/diary`, data)
+  api.post(DIARY_BASE, data)
 
 export const updateEntry   = (id, data) =>
-  axios.put(`${BASE}/diary/${id}`, data)
+  api.put(`${DIARY_BASE}/${id}`, data)
 
 export const deleteEntry   = (id) =>
-  axios.delete(`${BASE}/diary/${id}`)
+  api.delete(`${DIARY_BASE}/${id}`)
 
 // Tasks 
 export const getTasks    = (internshipId) =>
-  axios.get(`${BASE}/tasks/internship/${internshipId}`)
+  api.get(`${TASKS_BASE}/internship/${internshipId}`)
 
 export const addTask     = (data) =>
-  axios.post(`${BASE}/tasks`, data)
+  api.post(TASKS_BASE, data)
 
 export const updateTask  = (id, data) =>
-  axios.put(`${BASE}/tasks/${id}`, data)
+  api.put(`${TASKS_BASE}/${id}`, data)
 
 export const deleteTask  = (id) =>
-  axios.delete(`${BASE}/tasks/${id}`)
+  api.delete(`${TASKS_BASE}/${id}`)
 
 //  Monthly Reports 
 export const getReports    = (internshipId) =>
-  axios.get(`${BASE}/monthly-reports/internship/${internshipId}`)
+  api.get(`${MONTHLY_REPORTS_BASE}/internship/${internshipId}`)
 
 export const addReport     = (data) =>
-  axios.post(`${BASE}/monthly-reports`, data)
+  api.post(MONTHLY_REPORTS_BASE, data)
 
 export const updateReport  = (id, data) =>
-  axios.put(`${BASE}/monthly-reports/${id}`, data)
+  api.put(`${MONTHLY_REPORTS_BASE}/${id}`, data)
 
 export const deleteReport  = (id) =>
-  axios.delete(`${BASE}/monthly-reports/${id}`)
+  api.delete(`${MONTHLY_REPORTS_BASE}/${id}`)
 
 // Final Report 
 export const getFinalReport    = (internshipId) =>
-  axios.get(`${BASE}/final-reports/internship/${internshipId}`)
+  api.get(`${FINAL_REPORTS_BASE}/internship/${internshipId}`)
 
 export const addFinalReport    = (data) =>
-  axios.post(`${BASE}/final-reports`, data)
+  api.post(FINAL_REPORTS_BASE, data)
 
 export const updateFinalReport = (id, data) =>
-  axios.put(`${BASE}/final-reports/${id}`, data)
+  api.put(`${FINAL_REPORTS_BASE}/${id}`, data)
 
 export const deleteFinalReport = (id) =>
-  axios.delete(`${BASE}/final-reports/${id}`)
+  api.delete(`${FINAL_REPORTS_BASE}/${id}`)
